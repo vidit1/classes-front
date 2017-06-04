424722f9bce3987f86884a828965827b15bad5eb
Vidit <vidit.gupta@jugnoo.in>
2017-06-04 17:03:28 +0530

    pointing changed

--- scripts/config.js
+++ scripts/config.js
@@ -2,5 +2,5 @@
  * Created by vidit on 3/6/17.
  */
 
-// var HOST_NAME = "https://arcane-sierra-54121.herokuapp.com/";
-var HOST_NAME = "http://localhost:8000/";
+var HOST_NAME = "https://arcane-sierra-54121.herokuapp.com/";
+// var HOST_NAME = "http://localhost:8000/";
